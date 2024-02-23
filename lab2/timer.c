49aa58ec7298d4dd49dd6525293ed543aafb2aef
Lucas Faria <up202207540@up,pt>
2024-02-23 21:11:10 +0000

    Added the timer_display_conf function and corrected bugs related to this function

--- lab2/timer.c
+++ lab2/timer.c
@@ -39,7 +39,7 @@ int (timer_get_conf)(uint8_t timer, uint8_t *st) {
 
   //Insert count and status (their active low!!!)
   rb_command |= TIMER_RB_COUNT_; //counter to 1
-  //rb_command ^= TIMER_RB_STATUS_;//status to 0
+
 
   //Insert Timer Value
   rb_command |= TIMER_RB_SEL(timer);
@@ -52,15 +52,66 @@ int (timer_get_conf)(uint8_t timer, uint8_t *st) {
   if(util_sys_inb(TIMER_0 + timer,st) != 0){
     return EXIT_FAILURE;
   }
-  printf("value = %x\n",*st);
   return EXIT_SUCCESS;
 
 }
 
 int (timer_display_conf)(uint8_t timer, uint8_t st,
                         enum timer_status_field field) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
 
-  return 1;
+  union timer_status_field_val conf;
+  uint8_t init = 0;
+
+  switch(field){
+    //configures the entire Byte
+    case tsf_all:
+      conf.byte = st;
+      break;
+
+    //configures the timer's initialization mode
+    case tsf_initial:
+      init =  ((st << 2) >> 6) & (0x03);
+      
+      switch (init)
+      {
+      case 1:
+        conf.in_mode = LSB_only;
+        break;
+      case 2: 
+        conf.in_mode = MSB_only;
+        break;
+      case 3:
+        conf.in_mode = MSB_after_LSB;
+        break;
+      default:
+        conf.in_mode = INVAL_val;
+        break;
+      }
+      
+      break;
+
+    //Configures the timer's counting mode
+    case tsf_mode:
+      conf.count_mode = ((st << 4) >> 5) & (0x07);
+      if(conf.count_mode == 6){
+        conf.count_mode = 2;
+      }
+      else if(conf.count_mode == 7){
+        conf.count_mode = 3;
+      }
+      printf("%u\n",conf.count_mode);
+      break;
+
+    //Configures the timer's counting base
+    case tsf_base:
+      if(st & TIMER_BCD){
+        conf.bcd = true;
+      }
+      else{
+        conf.bcd = false;
+      }
+      break;
+  }
+
+  return timer_print_config(timer, field, conf);
 }
