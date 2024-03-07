d8111795eb3f089dbcc8893bbf9537512ef7ee7f
Lucas Faria <up202207540@up,pt>
2024-03-07 11:38:52 +0000

    added the macros to use in the lab3

---
+++ lab3/i8042.h
@@ -0,0 +1,46 @@
+#ifndef _LCOM_I8042_H_
+#define _LCOM_I8042_H_
+
+#include <lcom/lcf.h>
+
+
+/** @defgroup i8042 i8042
+ * @{
+ *
+ * Constants for programming the KBC
+ */
+
+/* I/O port adresses*/
+
+#define KBC_OUT_BUF   0x60    /*Out buffer (to read scancodes from the keyboard and return values)*/
+#define KBC_ST_REG    0x64    /*Used to read the KBC state*/
+#define KBC_CMD_REG   0x64    /*Used to send commands to the KBC*/
+
+/* Status Register */
+
+#define KBC_PARITY    BIT(7)  /*Parity error - invalid data*/
+#define KBC_TIMEOUT   BIT(6)  /*Timeout error - invalid data*/
+#define KBC_AUX       BIT(5)  /*Mouse data*/
+#define KBC_INH       BIT(4)  /*Inhibit flag: 0 if keyboard is inhibited*/
+#define KBC_A2        BIT(3)  /*A2 input line: irrelevant for LCOM*/
+#define KBC_SYS       BIT(2)  /*System flag: irrelevant for LCOM*/
+#define KBC_IBF       BIT(1)  /*Input buffer full dont write commands or arguments*/
+#define KBC_OBF       BIT(0)  /*Output buffer full - data available for reading*/
+
+/* KBC Commands */
+
+#define KBC_RD_CM_B   0x20    /*Read Command Byte*/
+#define KBC_WRT_CM_B  0x60    /*Write Command Byte*/
+#define KBC_CHECK     0xAA    /*Check KBC (Self-test)*/
+#define KDB_KB_INTER  0xAB    /*Check Keyboard Interface*/
+#define KDB_DISABLE_INTER   0xAD  /*Disable KBD Interface*/
+#define KDB_ENABLE_INTER    0xAE  /*Enable KBD Interface*/
+
+
+#define KBD_ESC_BREAK   0x81     /*ESC breakcode*/
+
+
+
+
+
+#endif
--- lab3/kbd.c
+++ lab3/kbd.c
@@ -1,1 +1,7 @@
 #include <lcom/lcf.h>
+
+static uint8_t scancode = 0;
+
+void (kbc_ih)() {
+    // to be completed
+}
